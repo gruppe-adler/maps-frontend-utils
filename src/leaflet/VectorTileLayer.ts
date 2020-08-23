de6033eb85263b6c8f4452798c29438b590310ff
DerZade <DerZade@gmail.com>
2020-08-23 14:52:35 +0200

    Load mapbox gl css

--- src/leaflet/VectorTileLayer.ts
+++ src/leaflet/VectorTileLayer.ts
@@ -1,5 +1,6 @@
 import 'mapbox-gl-leaflet';
 import '../types/mapbox-gl-leaflet';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import { Style as MapboxStyle } from 'mapbox-gl';
 // @ts-ignore
 import { MapboxGL } from 'leaflet';
